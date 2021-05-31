c9a1a85573183b8ef2949ec82a46cda8801eeb36
RuchiS26 <singhal.rchi@gmail.com>
2021-05-31 18:32:55 +0530

    Modified config and database files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
+    mongoose.connect(process.env.DB_LOCAL_URI, { // if online db is used write process.env.DB_URI 
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
