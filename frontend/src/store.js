bd3a2d102054a300b17fe641bfff4431ab8260e2
RuchiS26 <singhal.rchi@gmail.com>
2021-05-31 07:41:20 +0530

    Added Frontend functionalities to Users, Products, Orders, Cart, Reviews and Payments

---
+++ frontend/src/store.js
@@ -0,0 +1,37 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { productsReducer, productDetailsReducer, newReviewReducer } from './reducers/productReducers';
+import { authReducer, userReducer, forgotPasswordReducer } from "./reducers/userReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from "./reducers/orderReducers";
+
+const reducer = combineReducers ({
+    products: productsReducer,
+    productDetails: productDetailsReducer,
+    newReview: newReviewReducer,
+    auth: authReducer,
+    user: userReducer,
+    forgotPassword: forgotPasswordReducer,
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer
+})
+
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+        shippingInfo: localStorage.getItem('shippingInfo')
+            ? JSON.parse(localStorage.getItem('shippingInfo'))
+            : {}
+    }
+}
+
+const middlware = [thunk];
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlware)))
+
+export default store;
